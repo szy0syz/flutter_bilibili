d4397b4bf841afb2ec12689e67a3db847652ca97
szy0syz <szy0syz@gmail.com>
2021-10-08 00:15:01 +0800

    feat: navigation_bar part1

---
+++ lib/widget/navigation_bar.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+enum StatusStyle { LIGHT_CONTENT, DARK_CONTENT }
+
+class NavigationBar extends StatelessWidget {
+  final StatusStyle statusStyle;
+  final Color color;
+  final double height;
+  final Widget child;
+
+  const NavigationBar(
+      {Key? key,
+      this.statusStyle = StatusStyle.DARK_CONTENT,
+      this.color = Colors.white,
+      this.height = 46,
+      required this.child})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    //状态栏高度
+    var top = MediaQuery.of(context).padding.top;
+
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      height: top + height,
+      child: child,
+      padding: EdgeInsets.only(top: top),
+      decoration: BoxDecoration(color: color),
+    );
+  }
+}
