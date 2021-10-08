2be02373e05ce59d17d0b7e285966c4b0b402621
szy0syz <szy0syz@gmail.com>
2021-10-08 14:46:21 +0800

    feat: 首页Grid布局

--- lib/page/home_tab_page.dart
+++ lib/page/home_tab_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bilibili/model/home_mo.dart';
 import 'package:flutter_bilibili/widget/hi_banner.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
 class HomeTabPage extends StatefulWidget {
   final String name;
@@ -16,11 +17,22 @@ class HomeTabPage extends StatefulWidget {
 class _HomeTabPageState extends State<HomeTabPage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: ListView(
-        children: [if (widget.bannerList != null) _banner()],
-      ),
-    );
+    return MediaQuery.removePadding(
+        removeTop: true,
+        context: context,
+        child: StaggeredGridView.countBuilder(
+            crossAxisCount: 2,
+            itemBuilder: (BuildContext context, int index) {
+              if (widget.bannerList != null && index == 0) {
+                return Padding(
+                  padding: EdgeInsets.only(bottom: 8),
+                  child: _banner(),
+                );
+              } else {
+                return Text("1");
+              }
+            },
+            staggeredTileBuilder: (int index) {}));
   }
 
   _banner() {
