a2c4dceab8939dedbd1786a77072c09d4b3281ae
szy0syz <szy0syz@gmail.com>
2021-10-08 22:15:03 +0800

    feat: 封装VideoCard

---
+++ lib/util/format_util.dart
@@ -0,0 +1,24 @@
+// 时间转换 将秒转换为 分钟:秒
+String durationTrasform(int seconds) {
+  int m = (seconds / 60).truncate();
+  int s = seconds - m * 60;
+
+  if (s < 10) {
+    return '$m:$s';
+  }
+
+  return '$m:$s';
+}
+
+// 数字转万
+String countFormat(int count) {
+  String views = "";
+
+  if (count > 9999) {
+    views = "${(count / 10000).toStringAsFixed(2)}万";
+  } else {
+    views = count.toString();
+  }
+
+  return views;
+}
--- lib/widget/video_card.dart
+++ lib/widget/video_card.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bilibili/model/video_model.dart';
+import 'package:flutter_bilibili/util/format_util.dart';
+import 'package:transparent_image/transparent_image.dart';
 
 class VideoCard extends StatelessWidget {
   final VideoModel videoModel;
@@ -10,7 +12,84 @@ class VideoCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return InkWell(
       onTap: () {},
-      child: Image.network(videoModel.cover),
+      child: SizedBox(
+        height: 200,
+        child: Card(
+          // 取消卡片默认边距
+          margin: EdgeInsets.only(left: 4, right: 4, bottom: 8),
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(5),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                _itemImage(context),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  _itemImage(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Stack(
+      children: [
+        FadeInImage.memoryNetwork(
+            // 默认宽度
+            width: size.width / 2 - 20,
+            height: 120,
+            placeholder: kTransparentImage,
+            image: videoModel.cover,
+            fit: BoxFit.cover),
+        Positioned(
+            left: 0,
+            right: 0,
+            bottom: 0,
+            child: Container(
+              padding: EdgeInsets.only(left: 8, right: 8, bottom: 3, top: 5),
+              decoration: BoxDecoration(
+                  // 渐变
+                  gradient: LinearGradient(
+                      begin: Alignment.bottomCenter,
+                      end: Alignment.topCenter,
+                      colors: [Colors.black54, Colors.transparent])),
+              child: Row(
+                children: [
+                  _iconText(Icons.ondemand_video, videoModel.view),
+                  _iconText(Icons.ondemand_video, videoModel.favorite),
+                  _iconText(null, videoModel.duration)
+                ],
+              ),
+            ))
+      ],
+    );
+  }
+
+  _iconText(IconData? iconData, int count) {
+    String views = "";
+    if (iconData != null) {
+      views = countFormat(count);
+    } else {
+      views = durationTrasform(videoModel.duration);
+    }
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        if (iconData != null)
+          Icon(
+            iconData,
+            color: Colors.white,
+            size: 12,
+          ),
+        Padding(
+          padding: EdgeInsets.only(left: 3),
+          child: Text(
+            views,
+            style: TextStyle(color: Colors.white, fontSize: 10),
+          ),
+        )
+      ],
     );
   }
 }
