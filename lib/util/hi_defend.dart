7088cc6ceda49558a385b435d49c979ef64dd137
szy0syz <szy0syz@gmail.com>
2021-11-22 17:55:53 +0800

    feat: hi_defend

--- lib/util/hi_defend.dart
+++ lib/util/hi_defend.dart
@@ -1,9 +1,21 @@
 import 'dart:async';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 class HiDefend {
   run(Widget app) {
+    //处理框架异常
+    FlutterError.onError = (FlutterErrorDetails details) async {
+      // 线上环境，走上报逻辑
+      if (kReleaseMode) {
+        Zone.current.handleUncaughtError(details.exception, details.stack!);
+      } else {
+        // 开发期间，走Console抛出
+        FlutterError.dumpErrorToConsole(details);
+      }
+    };
+
     runZonedGuarded(() {
       runApp(app);
     }, (e, s) => _reportError(e, s));
